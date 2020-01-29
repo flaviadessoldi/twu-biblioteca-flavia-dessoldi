84ef2683b368fb886cd31c38056270a33c5c090c
flavia.dessoldi <flavia.dessoldi@thoughtworks.com>
2020-01-29 13:43:16 -0300

    [RELEASE 1 - 1.5]Create invalid option

--- test/book/BookListTest.java
+++ test/book/BookListTest.java
@@ -1,16 +1,35 @@
 package book;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class BookListTest {
+
+
+    private PrintStream sysOut;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        sysOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void revertStreams() {
+        System.setOut(sysOut);
+    }
 
     @Test
     public void shouldShowBookList(){
@@ -21,8 +40,10 @@ public class BookListTest {
 
         List<Book> listBook = Arrays.asList(book1);
 
+        bookList.printBookList(listBook);
 
-       assertNotNull(listBook);
+
+       assertThat(outContent.toString(), containsString("Harry Potter"));
 
     }
 }
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,16 +1,35 @@
 package com.twu.biblioteca;
 
 import book.BookList;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.junit.Assert.*;
 
 public class BibliotecaAppTest {
 
     BibliotecaApp bibliotecaApp = new BibliotecaApp();
+
+    private PrintStream sysOut;
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        sysOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void revertStreams() {
+        System.setOut(sysOut);
+    }
 
     @Test
     public void shouldShowWelcomeMessage(){
@@ -18,19 +37,37 @@ public class BibliotecaAppTest {
 
         bibliotecaApp.welcomeMessage("Welcome to Biblioteca!");
 
-        assertTrue("Welcome to Biblioteca!", true);
+        assertEquals(outContent.toString(), "Welcome to Biblioteca!");
 
     }
 
     @Test
 
     public void shouldShowMenuOptions(){
+        bibliotecaApp.showMenuOptions();
+
+        assertThat(outContent.toString(), containsString("1 - List books"));
 
 
     }
 
     @Test
-    public void shouldSelectOption(){
+    public void shouldSelectCorrectOption(){
+
+        bibliotecaApp.chooseOption(1);
+
+        assertThat(outContent.toString(), containsString("Author"));
+
+
+    }
+
+    @Test
+    public void shouldShowMessageWhenChooseInvalidOption(){
+
+        bibliotecaApp.chooseOption(8);
+
+        assertThat(outContent.toString(), containsString("Select a valid option!"));
+
 
     }
 
